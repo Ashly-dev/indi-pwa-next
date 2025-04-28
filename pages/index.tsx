a33baac6f5b1a8143956f5809949cc07072cd76f
Shama Razak <44234784+shamarazak@users.noreply.github.com>
2025-04-28 18:35:46 +0530

    test index file issue

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,37 +1,51 @@
-import Banner from "@/Components/Banner";
-import Featured from "@/Components/Featured";
-import Layout from "@/Components/Layout";
-import OfferHighlight from "@/Components/OfferHighlight";
+// import Banner from "@/Components/Banner";
+// import Featured from "@/Components/Featured";
+// import Layout from "@/Components/Layout";
+// import OfferHighlight from "@/Components/OfferHighlight";
+// import Head from "next/head";
+// import { GetServerSideProps } from "next";
+
+// interface HomeProps {
+//   themeFolder: string;
+// }
+
+// export default function Home({ themeFolder }: HomeProps) {
+//   return (
+//     <>
+//       <Head>
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//         <title>Indibet | Landing</title>
+//       </Head>
+//       <Layout>
+//         <Banner themeFolder={themeFolder} />
+//         <Featured />
+//         <OfferHighlight />
+//       </Layout>
+//     </>
+//   );
+// }
+
+// export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+//   const cr = query.cr as string | undefined;
+//   const themeFolder = cr || "default"; // fallback
+
+//   return {
+//     props: {
+//       themeFolder,
+//     },
+//   };
+// };
 import Head from "next/head";
-import { GetServerSideProps } from "next";
 
-interface HomeProps {
-  themeFolder: string;
-}
-
-export default function Home({ themeFolder }: HomeProps) {
+export default function Home() {
   return (
     <>
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Indibet | Landing</title>
+        <title>Welcome Home</title>
       </Head>
-      <Layout>
-        <Banner themeFolder={themeFolder} />
-        <Featured />
-        <OfferHighlight />
-      </Layout>
+      <div style={{ padding: "100px", textAlign: "center" }}>
+        <h1>Welcome to Indibet Landing Page!</h1>
+      </div>
     </>
   );
 }
-
-export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-  const cr = query.cr as string | undefined;
-  const themeFolder = cr || "default"; // fallback
-
-  return {
-    props: {
-      themeFolder,
-    },
-  };
-};
